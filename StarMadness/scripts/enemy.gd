138a646479a4516d8223f07dcecaab40db3819d4
Elena Guzman Blanco <elena.guzbla@gmail.com>
2019-10-25 13:14:58 +0200

    Add damage on impact

--- StarMadness/scripts/enemy.gd
+++ StarMadness/scripts/enemy.gd
@@ -4,6 +4,8 @@ export var velocity = Vector2()
 
 export var armor = 2 setget set_armor
 
+var flare_preload = preload("res://scenes/items/flare.tscn")
+
 func _ready():
 	set_process(true)
 
@@ -17,3 +19,15 @@ func set_armor(value):
 	armor = value
 	if armor <= 0:
 		queue_free()
+
+func _on_Enemy_area_entered(area):
+	if area.is_in_group("spaceship"):
+		area.armor -= 1
+		show_flare()
+		queue_free()
+
+func show_flare():
+	var new_flare = flare_preload.instance()
+	new_flare.global_position = position
+	
+	get_parent().add_child(new_flare)
